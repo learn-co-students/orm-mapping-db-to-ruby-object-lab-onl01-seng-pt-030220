8550367ba3d47be7600e5ab31de871588a07f316
VQuine <01010110.v@gmail.com>
2020-06-02 22:08:42 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Student
   attr_accessor :id, :name, :grade
 
@@ -13,11 +15,30 @@ class Student
   def self.all
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
+    sql <<-SQL
+     SELECT * FROM students;
+    SQL
+
+    all_students = DB[:conn].execute(sql)
+
   end
 
   def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
+    sql = <<-SQL
+    SELECT * 
+    FROM students 
+    WHERE name = ?
+    LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+    #?I really don't understand why we're doing this
+    #? or how finding by a name should create instances???
+    #? absolutely lost but following pattern..
   end
   
   def save
@@ -45,4 +66,34 @@ class Student
     sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
   end
+
+def self.all_students_in_grade_9
+ sql = <<-SQL
+  SELECT *
+  FROM students
+  WHERE grade = '9'
+ SQL
+ DB[:conn].execute(sql).map
 end
+
+def self.students_below_12th_grade
+  sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE grade != '12'
+  SQL
+  DB[:conn].execute(sql).map do |row|
+    self.new_from_db(row)
+  end
+end
+
+
+
+
+
+
+
+
+
+  
+end #!classEND
